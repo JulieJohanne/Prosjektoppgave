4c4ca5fbed3f4db1863302db74b6d2eea7a53468
Unknown <jju95@live.no>
2018-12-05 14:02:24 +0100

    Merge

--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
 # import pandas
@@ -6,28 +7,40 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 # from keras.layers import LSTM
 # from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
 
 
 # Set seed
 seed = 7
 np.random.seed(seed)
 
-
 # Prepare data
 X = np.loadtxt('input_new.txt')
 Y = np.array(np.loadtxt('output_new.txt'))
 data = np.column_stack((X, Y))
 np.random.shuffle(data)
+<<<<<<< HEAD
 X = data[:, :-1]  # [data[:, 0], data[:, 1], data[:, 2], data[:, 3], data[:, 4]]
 Y = data[:, -1]
 
 trainXX = X[:int(np.round(0.8*len(X))), :]
+=======
+X = np.array([data[:, 0], data[:, 1]/data[:, 2], data[:, 3], data[:, 4]/252]).T  #data[:, :-1]
+Y = np.array(data[:, -1]/data[:, 2])
+# standardised_X = preprocessing.scale(X)
+# normalised_X = preprocessing.normalize(X)
+
+"""
+trainX = X[:int(np.round(0.8*len(X))), :]
+>>>>>>> master
 trainY = Y[:int(np.round(0.8*len(X)))]
 testXX = X[int(np.round(0.8*len(X)))+1:, :]
 testY = Y[int(np.round(0.8*len(X)))+1:]
+<<<<<<< HEAD
 
 trainX = [trainXX[:, 0], trainXX[:, 1], trainXX[:, 2], trainXX[:, 3], trainXX[:, 4]]
 testX = [testXX[:, 0], testXX[:, 1], testXX[:, 2], testXX[:, 3], testXX[:, 4]]
@@ -40,24 +53,37 @@ layer1 = 100
 layer2 = 100
 layer3 = 100
 #layer4 = 100
+=======
+"""
+
+# Create model
+input_size = np.shape(X)[1]
+output_size = 1 #np.shape(Y)[1]c
+layers = [40, 40, 40, 40]
+>>>>>>> master
 activation_function = 'relu'
 output_function = 'sigmoid'
 
 model = Sequential()
-model.add(Dense(layer1, input_dim=input_size, activation=activation_function))
-model.add(Dense(layer2, activation=activation_function))
-model.add(Dense(layer3, activation=activation_function))
-#model.add(Dense(layer4, activation=activation_function))
+model.add(Dense(layers[0], input_dim=input_size, activation=activation_function))
+model.add(Dropout(0.4))
+model.add(Dense(layers[1], activation=activation_function)) 
+model.add(Dropout(0.4))
+model.add(Dense(layers[2], activation=activation_function))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+model.add(Dropout(0.3))
+model.add(Dense(layers[3], activation=activation_function))
+model.add(Dropout(0.2))
 model.add(Dense(output_size, activation=output_function))
 
 model.summary()
 
 # Compile model
-loss_function = 'binary_crossentropy'  # 'mean_squared_error' 
-optimizing_algorithm = 'nadam'  # 'sdg'  # adam'
-model.compile(loss=loss_function, optimizer=optimizing_algorithm, metrics=['accuracy'])
+loss_function = 'mean_squared_error'  # 'binary_crossentropy' 'binary_crossentropy' 
+optimizing_algorithm = 'adam'  # 'nadam'  # 'sdg'
+model.compile(loss=loss_function, optimizer=optimizing_algorithm, metrics=['mse', 'mae', 'mape'])
 
 # Fit the model
+<<<<<<< HEAD
 no_epochs = 50
 batch_size = 32  # None  # 10
 validation_split = 0.8  # 0.67 None
@@ -65,18 +91,28 @@ validation_split = 0.8  # 0.67 None
 
 history = model.fit(trainX, trainY, epochs=no_epochs, batch_size=batch_size,  verbose=2, callbacks=None,
         validation_split=validation_split, validation_data=None, shuffle=True, class_weight=None,
+=======
+no_epochs = 100
+batch_size = 128  # 32  # None  # 10
+validation_split =  0.2 # None
+validation_data =  None #(testX,testY) # None
+history = model.fit(X, Y, epochs=no_epochs, batch_size=batch_size,  verbose=2, callbacks=None,
+        validation_split=validation_split, validation_data=validation_data, shuffle=True, class_weight=None,
+>>>>>>> master
         sample_weight=None, initial_epoch=0, steps_per_epoch=None, validation_steps=None)
+"""
 model.evaluate(trainX, trainY, batch_size=batch_size, verbose=1, sample_weight=None, steps=None)
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
 trainScore = np.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
 testScore = np.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
-
+"""
+print(history.history.keys())
 # Summarize history for accuracy
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
+plt.plot(history.history['mean_absolute_percentage_error'])
+plt.plot(history.history['val_mean_absolute_percentage_error'])
+plt.title('model absolute percentage error')
+plt.ylabel('error')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
@@ -89,7 +125,7 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
-
+"""
 # Make predictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
@@ -100,3 +136,4 @@ print('Test Score: %.2f RMSE' % (testScore))
 # round predictions
 # rounded = [round(x[0]) for x in predictions]
 # print(rounded)
+"""
